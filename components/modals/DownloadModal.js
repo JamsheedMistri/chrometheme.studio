7a0d632378cac71c55911d89a9e7d6b377c0ac98
Jamsheed Mistri <jmistri@berkeley.edu>
2021-04-03 22:16:33 -0700

    Update download modal to include link to YouTube video, source code, and more intuitive styling

--- components/modals/DownloadModal.js
+++ components/modals/DownloadModal.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { GlobalContext } from '../../context/GlobalContext';
 import Button from '@material-ui/core/Button';
 import Modal from '../Modal';
+import styles from '../../styles/modals/DownloadModal.module.css';
 
 const DownloadModal = React.memo(() => {
 	const { modals, updateModal } = useContext(GlobalContext);
@@ -10,17 +11,17 @@ const DownloadModal = React.memo(() => {
 	return (
 		<Modal open={modals.download} onClose={closeDownloadModal}>
 			<h1>Theme Installation Instructions</h1>
-			<p>Follow these instructions in order to start using your new theme.</p>
+			<p>Follow these instructions in order to start using your new theme. If you are having trouble following the instructions, you can also view the tutorial video by <a href='https://www.youtube.com/watch?v=9MnJO3xX1jI' className={styles.link} target='_blank'>clicking here</a>.</p>
 			<ol>
 				<li>
 					<p>Click the file at the bottom of your screen to un-zip it.</p>
 				</li>
 				<li>
-					<p>Open a new tab and go to URL <b>chrome://extensions</b> (or <b>edge://extensions</b> if you're on Edge).</p>
+					<p>Open a new tab and go to URL <span className={styles.link}>chrome://extensions</span> (or <span className={styles.link}>edge://extensions</span> if you're on Edge).</p>
 				</li>
 					<li>
 						<p>At the top right, slide the switch that says <b>Developer mode</b> to turn on Developer Mode.</p>
-						<p>Don't worry, this is only a theme, not an extension, so it can't do anything to your browser. (feel free to inspect the contents of the extension or the source code of this website if you don't believe me)</p>
+						<p>Don't worry, this is only a theme, not an extension, so it can't do anything to your browser. (feel free to inspect the contents of the extension or the <a href='https://github.com/JamsheedMistri/chrometheme.studio' className={styles.link} target='_blank'>source code</a> of this website if you don't believe me)</p>
 					</li>
 				<li>
 					<p>At the top left, click the <b>Load unpacked</b> button. Find the newly unzipped folder that you downloaded and select it.</p>
